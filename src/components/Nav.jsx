df6578483fd0836884dcea62820c8615afb44616
Matthew Lowe <matthewjlowe11@gmail.com>
2022-08-15 13:01:38 -0400

    Remove unneeded files, setup basic home pages and styled-components

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+//Import styled from styled-components and you can store all youre styles within the Container(Div)
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+
+`
+
+const Nav = () => {
+  return (
+    <Container>
+        Nav
+    </Container>
+  )
+}
+
+export default Nav
